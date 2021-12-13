3d5d6b0575f49e146ac7b6f7e16cb35cc836588f
nonnax <ironald@gmail.com>
2021-12-13 20:20:31 +0800

    update: wls.rb

--- wls.rb
+++ wls.rb
@@ -3,40 +3,22 @@
 # require 'gdbm'
 # require 'rubytools/string_ext'
 # 
-key=ARGV.first
-
-# GDBM.open('wup.db') do |db|
-  # db
-    # .to_h
-    # .keys
-    # .select{|k| /#{key}/.match(k)}
-    # .each do |kmatched|
-      # puts kmatched
-      # puts "-[#{kmatched.split(':').first}]".rjust(80, '-')
-      # 
-      # v=db[kmatched]
-      # v=v.decode64 if kmatched.match(/^safe/)
-      # puts v
-      # puts
-    # end
-# end
-
 require_relative 'wup/wup'
 
-wup=Wup.new
+key=ARGV.first
 
-wup.gdbm do |db|
-  db
-    .to_h
-    .keys
-    .select{|k| /#{key}/.match(k)}
-    .each do |kmatched|
-      puts kmatched
-      puts "-[#{kmatched.split(':').first}]".rjust(80, '-')
-      
-      v=db[kmatched]
-      v=wup.to_decode(kmatched, v)
-      puts v
-      puts    
-    end  
+Wup.new.tap do |wup|
+  wup 
+  .to_h
+  .keys
+  .select{|k| /#{key}/.match(k)}
+  .each do |kmatched|
+    puts kmatched
+    puts "-[#{kmatched.split(':').first}]".rjust(80, '-')
+    
+    v=wup[kmatched]
+    v=wup.to_decode(kmatched, v)
+    puts v
+    puts    
+  end  
 end
