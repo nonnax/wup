5a9f37af0b4b12ded18331c75a9314976041dd2a
nonnax <ironald@gmail.com>
2021-12-13 21:07:34 +0800

    update: win.rb

--- win.rb
+++ win.rb
@@ -3,11 +3,11 @@
 require_relative 'wup/wup'
 
 Wup.new.tap do |wup|
-  p wup.get do |t|
+  wup.get do |t|
     # decouples the editor
     # other methods are allowed to update it. 
     # i.e. from a stream source to update to the db
     IO.editor(t)
   end
-  # p wup.latest_post
+  puts wup.latest_post
 end
