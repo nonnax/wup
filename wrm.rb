adb043314fe5316ab74553b4cf7e9de0c0f83133
nonnax <ironald@gmail.com>
2021-12-13 21:50:32 +0800

    update: wrm.rb

--- wrm.rb
+++ wrm.rb
@@ -1,14 +1,13 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2021-12-12 22:01:16 +0800
-require 'gdbm'
-require 'rubytools/pipe_argv'
+require_relative 'wup/wup'
 
 key=PIPE_ARGV().dup
 
 exit unless key
 key.chomp!
 
-GDBM.open('wup.db') do |db|
+Wup.new.tap do |db|
    p db.delete(key) if db.key?(key) 
    db.reorganize
 end
