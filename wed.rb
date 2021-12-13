5d8701edbe65b8058dcb24aa709dfa1c9fbf0262
nonnax <ironald@gmail.com>
2021-12-13 16:06:52 +0800

    update: wed.rb

---
+++ wed.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# Id$ nonnax 2021-12-12 19:35:05 +0800
+require_relative 'wup/wup'
+
+key=PIPE_ARGV().dup
+
+exit if key.nil?
+key.chomp!
+
+
+Wup.new.edit(key) do |text|
+  IO.editor(text).chomp
+end
