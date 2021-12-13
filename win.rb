bfffc0fdca4e4f8426b1770a5d66e8346472dc05
nonnax <ironald@gmail.com>
2021-12-13 21:05:26 +0800

    update: win.rb

--- win.rb
+++ win.rb
@@ -2,13 +2,12 @@
 # Id$ nonnax 2021-12-13 14:45:28 +0800
 require_relative 'wup/wup'
 
-blook=Wup.new
-blook.get do |t|
-  # decouples the editor
-  # other methods are allowed to update it. 
-  # i.e. from a stream source to update to the db
-  IO.editor(t)
+Wup.new.tap do |wup|
+  p wup.get do |t|
+    # decouples the editor
+    # other methods are allowed to update it. 
+    # i.e. from a stream source to update to the db
+    IO.editor(t)
+  end
+  # p wup.latest_post
 end
-
-# blook.save
-p blook.latest_post
