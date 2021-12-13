2f81dc1b360d7367a327c88453fe73fae5b45909
nonnax <ironald@gmail.com>
2021-12-13 19:49:00 +0800

    update: wls.rb

--- wls.rb
+++ wls.rb
@@ -4,14 +4,19 @@ require 'gdbm'
 require 'rubytools/string_ext'
 
 key=ARGV.first
+
 GDBM.open('wup.db') do |db| 
-  db.keys.select{|k| /#{key}/.match(k)}.each do |like_key|
-    puts like_key
-    puts "-[#{like_key.split(':').first}]".rjust(80, '-')
-    
-    v=db[like_key]
-    v=v.decode64 if like_key.match(/^safe/)
-    puts v
-    puts    
-  end
+  db
+    .to_h
+    .keys
+    .select{|k| /#{key}/.match(k)}
+    .each do |like_key|
+      puts like_key
+      puts "-[#{like_key.split(':').first}]".rjust(80, '-')
+      
+      v=db[like_key]
+      v=v.decode64 if like_key.match(/^safe/)
+      puts v
+      puts    
+    end
 end
